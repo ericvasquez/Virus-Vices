2c22a955449649de337f3c4e7ec7de38ecb07436
f00 <dustinalandzes@drchrono.com>
2020-05-02 18:05:55 -0500

    remove useless readme, change eslint to use prettier and const/let

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
 const mysql = require("mysql");
+
 let connection;
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection = mysql.createConnection({
-		host: "localhost",
-		PORT: 3306,
-		user: "root",
-		password: "",
-		database: "cards_db",
-	});
+  connection = mysql.createConnection({
+    host: "localhost",
+    PORT: 3306,
+    user: "root",
+    password: "",
+    database: "cards_db",
+  });
 }
 
 connection.connect(function (err) {
-	if (err) throw err;
-	console.log(`connection id ${connection.threadId}`);
+  if (err) throw err;
+  console.log(`connection id ${connection.threadId}`);
 });
 module.exports = connection;
