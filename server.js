b842f61bc82042ced9352340af1b738e70b762e0
ArgentinaG <59855898+ArgentinaG@users.noreply.github.com>
2020-05-06 18:48:19 -0500

    updated server file

--- server.js
+++ server.js
@@ -105,5 +105,5 @@ app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 // db.sequelize.sync().then(function () {
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // });
