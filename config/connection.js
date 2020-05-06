19f4305bb43078e16982c3d3cb36bfece0cc5c17
Sylvia Soto <sylviadiane69@gmail.com>
2020-05-05 19:41:02 -0500

    changes in the html branck

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-
-var Sequelize = require("sequelize");
-
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("cards_db", "root", "ghm766", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-=======
 const mysql = require("mysql");
 
 let connection;
@@ -31,7 +16,6 @@ if (process.env.JAWSDB_URL) {
 connection.connect(function (err) {
   if (err) throw err;
   console.log(`connection id ${connection.threadId}`);
->>>>>>> master
 });
 
 // Exports the connection for other files to use
